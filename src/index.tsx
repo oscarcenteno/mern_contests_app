7649dc8e748053799c3ccc15c7c6da99ab75b83b
Oscar Centeno <oscar.centeno@outlook.com>
2023-04-13 18:52:57 +0200

    components and props

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 // react-dom
 import { createRoot } from "react-dom/client";
 
-const TODOS = ["Learn react", "build something"];
+const Header = ({ message }) => <div className="header">{message}</div>;
 
 const App = () => {
   return (
-    <div className="container" title="Hello React">
-      <h1>Hello React</h1>
-      <h2 style={{ color: "green" }}>with JSX</h2>
-      {Math.random()}
-      {TODOS.map((e) => {
-        return <li>{e}</li>;
-      })}
+    <div className="container">
+      <Header message="Naming contests" />
     </div>
   );
 };
