6d1a95a6b5dd1a15319d1a507371d055d172f7b6
Oscar Centeno <oscar.centeno@outlook.com>
2023-04-13 18:48:38 +0200

    JSX Syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 // react-dom
 import { createRoot } from "react-dom/client";
 
+const TODOS = ["Learn react", "build something"];
+
 const App = () => {
-  return "<div>Hello React</div>";
+  return (
+    <div className="container" title="Hello React">
+      <h1>Hello React</h1>
+      <h2 style={{ color: "green" }}>with JSX</h2>
+      {Math.random()}
+      {TODOS.map((e) => {
+        return <li>{e}</li>;
+      })}
+    </div>
+  );
 };
 
 const container = document.getElementById("app");
