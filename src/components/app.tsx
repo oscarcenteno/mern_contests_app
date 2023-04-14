c446cfb925b6d608dd7672583c0173260332aacb
Oscar Centeno <oscar.centeno@outlook.com>
2023-04-14 12:09:56 +0200

    useEffect

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,22 @@
 import Header from "./header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+
+  // do anything that is a side effect to the component
+  // anything outside React
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log("set interval");
+      setCounter(counter + 1);
+    }, 1000);
+    return () => {
+      //do clean up for any asynchonous calls
+      clearInterval(intervalId);
+      console.log("clearInterval");
+    };
+  });
 
   return (
     <div className="container">
