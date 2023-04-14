99889430b05e0674bb714890d48c350d808387dc
Oscar Centeno <oscar.centeno@outlook.com>
2023-04-14 11:39:51 +0200

    state and event handlers

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,20 @@
 import Header from "./header";
+import { useState } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="container">
       <Header message="Naming contests" />
+      <button
+        type="submit"
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        {counter}
+      </button>
     </div>
   );
 };
